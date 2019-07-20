2db850f262e28a6068b1e82152655f3fb99e8a0c
Adrijaned <adrijaned@gmail.com>
2019-07-20 22:47:24 +0200

    Added StringStore stump

---
+++ src/include/StringStore.hpp
@@ -0,0 +1,60 @@
+/*
+	The ALB Programming Language
+	ALB Developers Team (C) 2019
+
+	This software is distributed under the MIT license
+	Visit https://github.com/albpl/compiler/blob/master/LICENSE for further details
+*/
+
+#ifndef ALB_STRINGSTORE_HPP
+#define ALB_STRINGSTORE_HPP
+
+#include <vector>
+#include <string>
+
+namespace alb_lang {
+  /**
+   * Provides a storage for all string literals in compilation target.
+   */
+  class StringStore {
+  public:
+    /**
+     * Returns the amount of currently stored string literals.
+     *
+     * @return The amount of string literals.
+     */
+    int getStringLiteralCount() const;
+
+    /**
+     * Stores a string in this storage.
+     *
+     * The returned id can be then used for querying the string again anytime later.
+     *
+     * @param s String to store.
+     * @return The newly assigned global id of the stored string literal.
+     */
+    std::string storeString(const std::string &s);
+
+    /**
+     * Returns the string literal stored under specific id.
+     *
+     * @param id The id for the string literal, as returned by \c storeString
+     * @return The actual string literal
+     */
+    std::string getString(const std::string &id) const;
+  };
+
+  int StringStore::getStringLiteralCount() const {
+    return 0;
+  }
+
+  std::string StringStore::storeString(const std::string &s) {
+    return std::string();
+  }
+
+  std::string StringStore::getString(const std::string &id) const {
+    return std::string();
+  }
+}
+
+#endif //ALB_STRINGSTORE_HPP
