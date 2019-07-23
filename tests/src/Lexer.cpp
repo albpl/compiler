dba75a2e5a31636876c73b3634fa6b5133f38bdd
Adrijaned <adrijaned@gmail.com>
2019-07-23 18:01:40 +0200

    Added simple string parsing test

--- tests/src/Lexer.cpp
+++ tests/src/Lexer.cpp
@@ -8,19 +8,21 @@
 
 #include <gtest/gtest.h>
 #include <Lexer.hpp>
+#include <absl/strings/match.h>
+#include <Resources.hpp>
 
-TEST(tokenizer_tests, tokenizer_tests_constructor_Test) {
+TEST(Lexer, constructor) {
   ASSERT_NO_THROW(alb_lang::Lexer{});
 }
 
-TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_token_count_Test) {
+TEST(Lexer, utf8_parsing_token_count) {
   char testData[] = "BEGIN .\nEND";
   std::vector<alb_lang::Token*> tokens;
   alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
   ASSERT_EQ(tokens.size(), 3);
 }
 
-TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_comment_Test) {
+TEST(Lexer, utf8_parsing_comment) {
   char testData[] = "BEGIN .\n// This is a comment\nEND";
   std::vector<alb_lang::Token*> tokens;
   alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
@@ -30,7 +32,7 @@ TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_comment_Test) {
   EXPECT_EQ(tokens[2]->getTextContents(), std::string{"END"});
 }
 
-TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_multiline_comment_Test) {
+TEST(Lexer, utf8_parsing_multiline_comment) {
   char testData[] = "BEGIN .\n/*This is\na comment*/END";
   std::vector<alb_lang::Token*> tokens;
   alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
@@ -38,4 +40,22 @@ TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_multiline_comment_Test) {
   EXPECT_EQ(tokens[0]->getTextContents(), std::string{"BEGIN"});
   EXPECT_EQ(tokens[1]->getTextContents(), std::string{"."});
   EXPECT_EQ(tokens[2]->getTextContents(), std::string{"END"});
+}
+
+TEST(Lexer, utf8_parsing_simple_string) {
+  char testData[] = "BEGIN .\nvar {testString \"randomText\"}\nEND";
+  std::vector<alb_lang::Token*> tokens;
+  alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
+  EXPECT_EQ(tokens.size(), 8);
+  ASSERT_GE(tokens.size(), 5);
+  EXPECT_EQ(tokens[0]->getTextContents(), std::string{"BEGIN"});
+  EXPECT_EQ(tokens[1]->getTextContents(), std::string{"."});
+  EXPECT_EQ(tokens[2]->getTextContents(), std::string{"var"});
+  EXPECT_EQ(tokens[3]->getTextContents(), std::string{"{"});
+  EXPECT_EQ(tokens[4]->getTextContents(), std::string{"testString"});
+  ASSERT_GE(tokens.size(), 7);
+  EXPECT_TRUE(absl::StartsWith(tokens[5]->getTextContents(), "::internal::strings::"));
+  EXPECT_EQ(alb_lang::Resources::stringStore().getString(tokens[5]->getTextContents()), std::string{"randomText"});
+  EXPECT_EQ(tokens[6]->getTextContents(), std::string{"}"});
+  EXPECT_EQ(tokens[7]->getTextContents(), std::string{"END"});
 }
